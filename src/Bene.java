b05bef670d816ec81d9bceaa0ac9b89d489674e2
DennisDoubek <dennis.doubek@gmail.com>
2024-12-09 09:58:36 +0100

    Up Bene

---
+++ src/Bene.java
@@ -0,0 +1,14 @@
+public class Bene {
+
+
+
+    // Wenn du das hier sehen kannst, dann ist auf jeden fall der letezt Commit erfolgreich gewesen
+
+
+
+    // Außerdem, sollten hier links noch 32 Weitere KLassen zu sehen sein
+
+
+    // Kuss auf die Nuss und Danke
+
+}
